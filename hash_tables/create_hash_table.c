73dfc0c0df76db2e1de1910e3b3d0dd6439395b8
shazaaly <shaza.aly@gmail.com>
2023-07-06 15:57:28 +0300

    structs

---
+++ hash_tables/create_hash_table.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "hash.h"
+#define HASH_TABLE_CAPACITY 5000
+
+/* allocate menory for item, item key, item value*/
+
+
+
+
+ /* allocating memory and setting its size, count, and items */
+
+
+
+int main(void)
+{
+
+	return (0);
+}
