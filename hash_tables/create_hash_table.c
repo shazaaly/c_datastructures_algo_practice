c35176600ea405927a504322cd4a3694663c3215
shazaaly <shaza.aly@gmail.com>
2023-07-06 16:02:08 +0300

    create hash table, hash table item prototype

--- hash_tables/create_hash_table.c
+++ hash_tables/create_hash_table.c
@@ -4,6 +4,18 @@
 #define HASH_TABLE_CAPACITY 5000
 
 /* allocate menory for item, item key, item value*/
+H_item *create_new_item(char *key, char *value)
+{
+	return (item);
+}
+
+
+HashTable *create_hash_table(int size, int count)
+{
+
+	return (table)
+}
+
 
 
 
--- hash_tables/hash.h
+++ hash_tables/hash.h
@@ -4,9 +4,8 @@
 typedef struct Hash_item{
 	char *str;
 	char *value
-} item;
+} H_item;
 
-#endif
 
 typedef struct HashTable{
 	// Contains an array of pointers to items.
@@ -15,3 +14,10 @@ typedef struct HashTable{
 	int count;
 
 } HashTable;
+
+
+H_item *create_new_item(char *key, char *value);
+HashTable *create_hash_table(int size, int count)
+
+
+#endif
